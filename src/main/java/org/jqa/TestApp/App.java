51596e5fe4b831225d468103b33aa57b79316968
Sean Campbell <rushdjava@Seans-MacBook-Pro.local>
2017-03-10 17:55:49 -0500

    Initial Github commit

---
+++ src/main/java/org/jqa/TestApp/App.java
@@ -0,0 +1,13 @@
+package org.jqa.TestApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
